1e8987d55082a0053a1c258c8a6750845adcdbf3
Leejungwoo <tkdeod1234@naver.com>
2022-02-11 15:12:09 +0900

    백준_기본 수학2_직각삼각형

---
+++ src/BasicMath2/Right_Triangle.java
@@ -0,0 +1,25 @@
+package BasicMath2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Right_Triangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[3];
+        while (true) {
+            arr[0] = sc.nextInt();
+            arr[1] = sc.nextInt();
+            arr[2] = sc.nextInt();
+            Arrays.sort(arr);
+            if (arr[0] == 0 && arr[1] == 0 && arr[2] == 0) {
+                break;
+            }else if (Math.pow(arr[2], 2) == Math.pow(arr[0], 2) + Math.pow(arr[1], 2)) {
+                System.out.println("right");
+            }
+            else {
+                System.out.println("wrong");
+            }
+        }
+    }
+}
