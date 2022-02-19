9540ff9c15c8a6093120da9631cf0bb9b36f6210
Leejungwoo <tkdeod1234@naver.com>
2022-02-19 17:38:32 +0900

    백준_입출력과 사칙연산_1998년생인 내가 태국에서는 2541년생?!

---
+++ src/com/leejeongwoo/scanner/Tai.java
@@ -0,0 +1,10 @@
+package com.leejeongwoo.scanner;
+
+import java.util.Scanner;
+
+public class Tai {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() - 543);
+    }
+}
