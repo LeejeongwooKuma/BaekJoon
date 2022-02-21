c0a1dcc5d3294cbc8ebddf39da670a3257e4720c
Leejungwoo <tkdeod1234@naver.com>
2022-02-21 14:53:03 +0900

    백준_기본 수학2_직각삼각형

--- src/com/leejeongwoo/basicmath2/Right_Triangle.java
+++ src/com/leejeongwoo/basicmath2/Right_Triangle.java
@@ -2,7 +2,6 @@ package com.leejeongwoo.basicmath2;
 
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class Right_Triangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
